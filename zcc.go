dd2e307b16af009fe6408e40a5d953b1f9574aea
Sergio Pereira <sergitopereira@hotmail.com>
2022-04-27 11:33:01 -0500

    ok

--- zcc.go
+++ zcc.go
@@ -21,7 +21,12 @@ type myToken struct {
 	Token string `json:"jwtToken"`
 }
 
-func authenticate(base_url string, client_id string, secret_key string) (string, error) {
+type Devices struct {
+	Udid string `json:"udid"`
+	User string `json:"user"`
+}
+
+func Authenticate(base_url string, client_id string, secret_key string) (string, error) {
 	url := base_url + "/auth/v1/login"
 	payload := auth{ApiKey: client_id, SecretKey: secret_key}
 	json_data, err := json.Marshal(payload)
@@ -69,20 +74,45 @@ func NewClient(BaseURL string, client_id string, client_secret string) (*Client,
 		return &Client{}, errors.New("failed to parse API URL")
 	}
 	//Getting access token
-	access_token, err := authenticate(BaseURL, client_id, client_secret)
+	access_token, err := Authenticate(BaseURL, client_id, client_secret)
 	if err != nil {
 		return &Client{}, err
 	}
 	return &Client{
 		BaseURL: BaseURL,
 		HTTPClient: &http.Client{
-		Timeout: time.Second * 10,
+			Timeout: time.Second * 10,
 		},
 		RetryMax: 10,
-		Token:access_token,
+		Token:    access_token,
 	}, nil
 
 }
 
+func (c *Client) FetchDevices(orgId int) ([]Devices, error) {
+	url := c.BaseURL + "/public/v1/getDevices"
+	client := http.Client{}
+	req , err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal("Error getting response. ", err)
+	}
+    req.Header.Set("auth-token", c.Token)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Error reading response. ", err)
+	}
+	defer resp.Body.Close()
+    body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	res := []Devices{}
+	err = json.Unmarshal(body, &res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
 
 
+}
